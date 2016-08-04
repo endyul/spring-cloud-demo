e263c85bc039ad4bf4cc349b0bdc68682051e6c7
endyul <zendyul@gmail.com>
2016-08-04 18:32:38 +0800

    add demo7

---
+++ demo7-spring_cloud_stream/GreetingSource/src/main/java/com/nvwashi/GreetingSourceApplication.java
@@ -0,0 +1,12 @@
+package com.nvwashi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingSourceApplication.class, args);
+	}
+}
---
+++ demo7-spring_cloud_stream/LoggingSink/src/main/java/com/nvwashi/LoggingSinkApplication.java
@@ -0,0 +1,12 @@
+package com.nvwashi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingSinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingSinkApplication.class, args);
+	}
+}
